fd6a478eeb1c7924a942c0e1c02cdaa54f3151cd
nmngarg174 <nmngarg174@gmail.com>
2015-10-07 08:05:18 +0530

    Running_2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -13,6 +13,7 @@ import java.io.*;
  *
  * @author Naman
  */
+ // make it aligned with the inputs here 
 public class SnakeLadder {
 
     /**
@@ -22,20 +23,23 @@ public class SnakeLadder {
         // TODO code application logic here
         
         int i,j,k,snakes=0,player=0; 
-        int [] board=new int[100];
+       // int [] board=new int[100];
         Scanner sc=new Scanner(System.in); 
         System.out.println("Welcome to Snake and Ladder game !!"); 
         
         System.out.println("Enter no of snakes");
         snakes=sc.nextInt(); 
         int pos[][]=new int[snakes][2]; 
-        
+        sc.nextLine(); 
         System.out.println("Enter the positions");
         
         for(i=0;i<snakes;i++){
             System.out.println("Enter for snake"+(i+1));
-            pos[i][0]=sc.nextInt(); 
-            pos[i][1]=sc.nextInt(); 
+            String line=sc.nextLine();
+            String []vector=line.split(",");
+         //   System.out.println(line); 
+            pos[i][0]=Integer.parseInt(vector[0]); 
+            pos[i][1]=Integer.parseInt(vector[1]); 
         }
         
         // ladders entry 
@@ -43,17 +47,19 @@ public class SnakeLadder {
         System.out.println("Enter no of ladders");
         ladders=sc.nextInt(); 
         int posL[][]=new int[ladders][2]; 
-        
+        sc.nextLine(); 
         System.out.println("Enter the positions");
         
         for(i=0;i<ladders;i++){
             System.out.println("Enter for ladder"+(i+1));
-            posL[i][0]=sc.nextInt(); 
-            posL[i][1]=sc.nextInt(); 
+            String line=sc.nextLine();
+            String []vector=line.split(","); 
+            posL[i][0]=Integer.parseInt(vector[0]); 
+            posL[i][1]=Integer.parseInt(vector[1]); 
         }
-        
+        //sc.nextLine(); 
         // Players Entry over here
-        int index; 
+        int index=1; 
         System.out.println("Enter no of players");
         player=sc.nextInt(); 
         String names[]=new String[player]; 
@@ -67,23 +73,33 @@ public class SnakeLadder {
         }
         
         System.out.println("Game Start !!");
-        boolean winner=false; 
+        boolean winner=false;
+        boolean valid=false; 
         while(!winner){
             
             for(i=0;i<player;i++){
                 // enter 1st player position 
-                System.out.println("Enter "+i+" player's dice no");
-                index=sc.nextInt(); 
+                System.out.println("Enter "+names[i]+" player's dice no");
+                valid=false; 
+                while(!valid){
+                 index=sc.nextInt(); 
+                if(index>6 || index<0){
+                    System.out.println("Enter valid input between 1 and 6");
+                    valid=false; 
+                }else{
+                    valid=true; 
+                }   
+                }
                 evaluatePos(index,position,i,pos,snakes,ladders,posL);
                 if(position[i]==100){
-                    System.out.println("Congratulations player "+i+" wins :)");
+                    System.out.println("Congratulations player "+names[i]+" wins :)");
                     System.exit(0); 
                 }else{
                     // print the positions of all players
                         System.out.println("The positons : "); 
 
                     for(j=0;j<player;j++){
-                        System.out.println("Player"+j+" ->"+position[j]); 
+                        System.out.println(names[j]+" ->"+position[j]); 
                     }
                 }
             }
@@ -98,7 +114,10 @@ public class SnakeLadder {
             int nextPos; 
             int prePos,i; 
             nextPos=index+position[player]; 
-            
+            if(nextPos>100){
+                System.out.println("This case is not valid , try in another turn !!");
+                return; 
+            }
             // check if any snake lies over that path 
             for(i=0;i<ladders;i++){
                 
